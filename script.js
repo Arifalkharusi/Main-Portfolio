df3f4a7b040415f925f0b3b70aee07a4abb0f8e7
Arif <arifalkharus@hotmail.com>
2023-03-27 23:04:18 +0100

    27/03/2023

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 // selectors
-const sliderShow = document.querySelectorAll(".slide-content");
-const silderLeftBtn = document.querySelector(".slider-icon-left");
-const silderRightBtn = document.querySelector(".slider-icon-right");
+
 const preLoader = document.querySelector(".pre-loader");
 const firstName = document.querySelector(".first-name");
 const lastName = document.querySelector(".last-name");
 const topScroll = document.querySelector(".scroll-top");
 const contactSection = document.getElementById("contact-section");
 const contactBtn = document.getElementById("contact-btn");
-
-// var
-let curSlide = 0;
-let timy;
 
 // func
 const preLoaderFunc = () => {
@@ -24,33 +18,6 @@ const preLoaderFunc = () => {
     }, 1000);
   }, 2000);
 };
-// excecuted on load
-
-// slider fuctions
-const slideShow = function (slide) {
-  sliderShow.forEach(
-    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-  );
-};
-
-const moveSlide = () => {
-  slideShow(curSlide);
-  timer();
-};
-
-// slider timer fuction (every 7 sec, next slide)
-const timer = function () {
-  // clears timer on each itteration
-  clearInterval(timy);
-  timy = setInterval(function () {
-    if (curSlide === sliderShow.length - 1) {
-      curSlide = 0;
-    } else {
-      curSlide++;
-    }
-    slideShow(curSlide);
-  }, 7000);
-};
 
 // events
 topScroll.addEventListener("click", () => {
@@ -62,18 +29,4 @@ contactBtn.addEventListener("click", (e) => {
   contactSection.scrollIntoView({ behavior: "smooth" });
 });
 
-silderLeftBtn.addEventListener("click", () => {
-  if (curSlide < 1) return;
-  curSlide--;
-  moveSlide();
-});
-
-silderRightBtn.addEventListener("click", () => {
-  if (curSlide === sliderShow.length - 1) return;
-  curSlide++;
-  moveSlide();
-});
-
 preLoaderFunc();
-timer();
-slideShow(0);
